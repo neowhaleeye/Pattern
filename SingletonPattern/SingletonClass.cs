4185657c368712a6d689511ab9a40cc87363d136
yunkwan <neowhaleeye@gmail.com>
2016-05-04 11:18:00 +1000

    Initial

---
+++ SingletonPattern/SingletonClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SingletonClass
+    {
+        private static SingletonClass _singletonobject;
+        private SingletonClass()
+        {
+
+        }
+
+        public static SingletonClass CreateObject()
+        {
+            if(_singletonobject == null)
+            {
+                _singletonobject = new SingletonClass();
+            }
+
+            return _singletonobject;
+        }
+    }
+}
